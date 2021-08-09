2e819b64e4c2c8bf65e995016cee51f43bf15c3f
lubnamariyam62 <lubnamariyam@vajro.com>
2021-08-09 23:35:12 +0530

    init commit

---
+++ app/src/main/java/com/example/developersays/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.developersays
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.developersays.ui.theme.DevelopersaysTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevelopersaysTheme{
+                MessageCard(Message("Developer Says", "Welcome to Jetpack Compose playlist"))
+            }
+
+
+        }
+    }
+}
+
+data class Message(val author: String , val body: String)
+
+
+@Composable
+fun MessageCard(msg : Message){
+    Row(modifier =  Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(id = R.drawable.profile_picture), contentDescription = "contact profile picture" , modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape))
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+
+
+        Column() {
+            Text(text = msg.author,
+            color = MaterialTheme.colors.secondaryVariant , style = MaterialTheme.typography.subtitle2)
+
+
+            Spacer(modifier = Modifier.width(4.dp))
+            androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium ,elevation = 1.dp) {
+                Text(text = msg.body , modifier = Modifier.padding(all = 4.dp), style = MaterialTheme.typography.body2)
+
+            }
+
+
+
+        }
+
+
+
+
+
+        }
+        }
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
+@Composable
+fun DefaultPreview() {
+    DevelopersaysTheme{
+        MessageCard(msg = Message("Developer Says", "Welcome to Jetpack Compose playlist"))
+    }
+
+}
